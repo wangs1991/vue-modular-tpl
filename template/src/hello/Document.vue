bc3f47938f5bb51327887116bb791a48ea742e35
王帅 <2538125068@qq.com>
2018-04-08 17:29:23 +0800

    添加节流演示公用方法

--- template/src/hello/Document.vue
+++ template/src/hello/Document.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="panel_content" class="content-warpper">
+    <div>
+      <input type="text" @keydown="typing" v-model="hello"/>
+    </div>
+    <div @click="typing">按钮</div>
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, animi, debitis eum fuga fugit illum impedit in iste, laudantium nihil obcaecati odit optio placeat sed veritatis vitae voluptatem. Id, ut!
   </div>
 </template>
@@ -12,10 +16,18 @@
       };
     },
     computed: {},
-    components: {},
-    watch: {},
+    methods: {
+      typing: (function () {
+        console.log(12, new Date());
+        return debounce(function () {
+          console.log(111);
+        }, 1000, false);
+      })(),
+    },
     mounted () {
-
+      window.addEventListener('mousemove', debounce(function () {
+        console.log(new Date(), 'mousemove');
+      }, 3000, true));
     }
   };
 </script>
